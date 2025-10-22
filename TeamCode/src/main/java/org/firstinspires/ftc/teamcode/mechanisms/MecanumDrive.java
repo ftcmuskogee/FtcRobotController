fbd5e390743281886b900dd37bf6603c354eebe9
ftcmuskogee <43355408+ftcmuskogee@users.noreply.github.com>
2025-10-21 19:32:41 -0500

    stuff for 2025 season

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MecanumDrive.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class MecanumDrive {
+    private DcMotor frontLeftMotor, backLeftMotor, froRightMotor, backRightMotor;
+    private IMU imu;
+
+    public void init(HardwareMap hwMap) {
+        frontLeftMotor = hwMap.get(DcMotor.class, "LF");
+        backLeftMotor = hwMap.get(DcMotor.class, "LB");
+        froRightMotor = hwMap.get(DcMotor.class, "RF");
+        backRightMotor = hwMap.get(DcMotor.class, "RB");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        froRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        imu = hwMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP);
+
+        imu.initialize(new IMU.Parameters(RevOrientation));
+    }
+
+    public void drive(double forward, double strafe, double rotate) {
+        double frontLeftPower = forward + strafe + rotate;
+        double backLeftPower = forward - strafe + rotate;
+        double frontRightPower = forward - strafe - rotate;
+        double backRightPower = forward + strafe - rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;
+
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        frontLeftMotor.setPower(maxSpeed * (frontLeftPower / maxPower));
+        backLeftMotor.setPower(maxPower * (backLeftPower / maxPower));
+        froRightMotor.setPower(maxSpeed * (frontRightPower / maxPower));
+        backRightMotor.setPower(maxSpeed * (backRightPower / maxPower));
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MecanumFieldOrientatedOpMode.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class MecanumFieldOrientatedOpMode extends OpMode {
+    MecanumDrive drive = new MecanumDrive();
+    double forward, strafe, rotate;
+
+    @Override
+    public void init() {
+        drive.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        forward = -gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        drive.drive(forward,strafe,rotate);
+    }
+}
