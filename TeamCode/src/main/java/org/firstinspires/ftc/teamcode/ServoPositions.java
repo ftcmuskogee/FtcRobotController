c15ac2d75c08ff66686e038cc098d95ef4b94c2c
ftcmuskogee <43355408+ftcmuskogee@users.noreply.github.com>
2025-10-30 14:50:10 -0500

    Update ServoPositions.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoPositions.java
@@ -6,14 +6,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="Set", group="Servo")
 public class ServoPositions extends LinearOpMode {
-    private Servo shooterReloader = null;
+    private Servo servo = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        shooterReloader = hardwareMap.get(Servo.class, "SR");
-
-        shooterReloader.setDirection(Servo.Direction.FORWARD);
+        Servo = hardwareMap.get(Servo.class, "servo");
+        Servo.setDirection(Servo.Direction.FORWARD);
 
         // Wait for the game to start (driver presses START)
         telemetry.addData("Status", "Initialized");
@@ -25,13 +24,13 @@ public class ServoPositions extends LinearOpMode {
 
             // show current position when CIRCLE is pressed
             if (gamepad1.aWasPressed()) {
-                telemetry.addData("0 value is...", shooterReloader.getPosition());
+                telemetry.addData("0 value is...", Servo.getPosition());
                 telemetry.update();
             }
 
             // show current position when SQUARE is pressed
             if (gamepad1.bWasPressed()) {
-                telemetry.addData("1 value is...", shooterReloader.getPosition());
+                telemetry.addData("1 value is...", Servo.getPosition());
                 telemetry.update();
             }
         }
