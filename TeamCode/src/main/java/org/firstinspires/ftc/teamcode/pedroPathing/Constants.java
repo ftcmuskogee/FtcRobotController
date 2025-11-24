f6b42523086e508647a7e0ddb3d4d44ed79c3e04
androidSupremacy <ftcmuskogee@gmail.com>
2025-11-24 10:06:30 -0600

    PP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -12,8 +12,17 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
 
-    // double pounds2kg = 0.453592 * robot pounds ;
-    public static FollowerConstants followerConstants = new FollowerConstants().mass(8.981);
+    /*  NOTICE --- The final line written under these static constants has to end in ";" and be the only one 'ended'.
+    The order of testing is as follows; V V V
+    localization(DriveEncoderConstants), tuning opmode(MecanumConstants, then FollowerConstants)
+    */
+    // pounds2kg = 0.453592 * robot pounds ;
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(8.981);
+            /*
+            .forwardZeroPowerAcceleration(deceleration on driver hub after tuning opmode(Forward Zero Power Acceleration))
+            .lateralZeroPowerAcceleration(deceleration on driver hub after tuning opmode(Lateral Zero Power Acceleration))
+            */
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(0.6)
@@ -25,6 +34,11 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+            /*  Tuning Opmode (after Localization Tuners VVV Bottom of DriveEncoderConstants VVV)
+            .xVelocity(velocity on driver hub after tuning opmode(Forward Velocity Tuner))
+            .yVelocity(velocity on driver hub after tuning opmode(Lateral Velocity Tuner))
+
+            */
 
     public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
             .rightFrontMotorName("RF")
@@ -35,8 +49,8 @@ public class Constants {
             .leftRearEncoderDirection(Encoder.FORWARD)
             .rightFrontEncoderDirection(Encoder.REVERSE)
             .rightRearEncoderDirection(Encoder.REVERSE)
-            .robotWidth(15) // guessed
-            .robotLength(17) // guessed
+            .robotWidth(14)
+            .robotLength(17)
             /*  Tuning localizer
             .forwardTicksToInches(multiplier on driver hub after localization(forwardtuner))
             .strafeTicksToInches(multiplier on driver hub after localization(lateraltuner))
